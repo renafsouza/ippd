a2b5df923861ef42334d64b0679964c87dd970f4
Renã Souza <rfsouza710@gmail.com>
2022-10-19 09:14:09 -0300

    Major algorithm rework

--- main.cpp
+++ main.cpp
@@ -8,13 +8,13 @@
 
 using namespace std;
 
-const int WIDTH = 640, FRAMES = 2, HEIGHT = 360, BLOCK_SIZE = 8, THRESHOLD = 5, MAX_BLOCK_MATCHES = 1;
+const int WIDTH = 640, FRAMES = 120, HEIGHT = 360, BLOCK_SIZE = 8;
 
 int main(){
     ifstream file ("file.yuv", ios::in|ios::binary|ios::ate);
     ofstream newFile ("newFile.yuv", ios::in|ios::binary|ios::ate);
     streampos size;
-    vector<pair<vector<pair<int, int>>,vector<pair<int, int>>>> matches;
+    // vector<pair<vector<pair<int, int>>,vector<pair<int, int>>>> matches;
     char * memBlock;
 
     if ( file.is_open() && newFile.is_open()) { // always check whether the file is open
@@ -25,50 +25,44 @@ int main(){
         file.close();
         byte frame1[HEIGHT][WIDTH];
 
-        #pragma omp parallel for
         // Iterate through each frame
+        #pragma omp parallel for
         for(int f=1;f<FRAMES;++f){
             vector<pair<int, int>> Ra, Rv;
-            // Iterate through each line of each frame
-            for(int i=0;i<HEIGHT-8;++i){
-                // Iterate through each column of each line
-                for(int j=0;j<WIDTH-8;++j){
-                    int blockMatches = 0;
+            for(int i=0;i<HEIGHT-BLOCK_SIZE;i+=BLOCK_SIZE){
+                for(int j=0;j<WIDTH-BLOCK_SIZE;j+=BLOCK_SIZE){
+                    pair<int, int> bestMatch;
+                    float bestDiffAvg = -1;
                     // Iterate through each line of each frame
-                    for(int i2=0;i2<HEIGHT-8;++i2){
+                    for(int i2=0;i2<HEIGHT-BLOCK_SIZE;++i2){
                         // Iterate through each column of each line
-                        for(int j2 =0;j2<WIDTH-8;++j2){
-                            bool match = true;
+                        for(int j2 =0;j2<WIDTH-BLOCK_SIZE;++j2){
+                            float diffAvg = 0;
                             // Iterate trough each line of an 8x8 block
                             for(int ib =0;ib<BLOCK_SIZE;ib++){
                                 // Iterate trough each column of an 8x8 block
                                 for(int jb =0;jb<BLOCK_SIZE;jb++){
-                                    if(abs(memBlock[(i+ib*WIDTH)+j+jb] - (byte)memBlock[f*345600+(i2+ib)*WIDTH+j2+jb]) > THRESHOLD){
-                                        match = false;
-                                        break;
-                                    } 
+                                    diffAvg += abs((int)memBlock[(f*345600+(i+ib)*WIDTH)+j+jb] - (int)memBlock[(i2+ib)*WIDTH+j2+jb]);
                                 }
-                                if(!match) break;
                             }
-                            if(match){
-                                Ra.push_back(pair<int,int>(j,i));
-                                Rv.push_back(pair<int,int>(j2,i2));
-                                ++blockMatches;
-                                j2+=8;
-                                for(int ib = 0;ib<BLOCK_SIZE;ib++){
-                                    for(int jb =0;jb<BLOCK_SIZE;jb++){
-                                        memBlock[f*345600+(i2+ib)*WIDTH+j2+jb] = memBlock[(i+ib*WIDTH)+j+jb];
-                                    }
-                                }
-                                break;
+                            diffAvg = diffAvg / 64;
+                            if(diffAvg<bestDiffAvg || bestDiffAvg==-1){
+                                bestMatch = pair<int, int>(i2,j2);
+                                bestDiffAvg = diffAvg;
                             }
                         }
-                        if(blockMatches >= MAX_BLOCK_MATCHES) break;
                     }
-                    if(blockMatches > 0) j+=8;
+                    // Ra.push_back(pair<int,int>(j,i));
+                    // Rv.push_back(bestMatch);
+                    for(int ib =0;ib<BLOCK_SIZE;ib++){
+                        // Iterate trough each column of an 8x8 block
+                        for(int jb =0;jb<BLOCK_SIZE;jb++){
+                            memBlock[f*345600+(i+ib)*WIDTH+j+jb] = memBlock[(bestMatch.first+ib)*WIDTH+bestMatch.second+jb];
+                        }
+                    }
                 }
             }
-            matches.push_back(pair<vector<pair<int, int>>,vector<pair<int, int>>>(Ra,Rv));
+            // matches.push_back(pair<vector<pair<int, int>>,vector<pair<int, int>>>(Ra,Rv));
         }
 
         newFile.write (memBlock, size);
